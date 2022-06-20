4a12e5ead89cb84c08d0d7d7fce7e48baf974d4c
mdlorey <mdlorey@gmail.com>
2022-06-20 16:16:08 -0400

    add navigation guard that prompts user to save progress if input value has changed

--- src/components/Exercise-2/Page-1.vue
+++ src/components/Exercise-2/Page-1.vue
@@ -12,17 +12,37 @@
 
 <script lang="ts" setup>
   import { computed, ref } from 'vue'
+  import { onBeforeRouteLeave } from 'vue-router'
   import RippleButton from '@/components/RippleButton.vue'
   import { useSavedInput } from '@/stores/savedInput'
 
   const savedInputStore = useSavedInput()
   const input = ref(savedInputStore.input ?? '')
 
+  const inputsBlank = computed(() => input.value === '' && !savedInputStore.input)
   const inputMatches = computed(() => input.value === savedInputStore.input)
 
   const save = (): void => {
     savedInputStore.save(input.value)
   }
+
+  const discard = (): void => {
+    input.value = savedInputStore.input ?? ''
+  }
+
+  onBeforeRouteLeave(() => {
+    // prompt the user to save their progress if input value does not match the stored value
+    if (!inputsBlank.value && !inputMatches.value) {
+      // eslint-disable-next-line no-alert
+      const answer = window.confirm('Do you really want to leave? You have unsaved changes! (Cancel to discard, OK to save)')
+      // cancel the navigation and stay on the same page
+      if (!answer) {
+        discard()
+      } else {
+        save()
+      }
+    }
+  })
 </script>
 
 <style lang="scss">
